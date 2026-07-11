81a00ecb420ac227a7778d79cfc20e64e3e21fe2
Quangdai2207 <daitran.inbox@gmail.com>
2026-07-12 04:12:35 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import Home from "./pages/Home";
 import List from "./pages/jobs/List";
-import EditJob from "./pages/jobs/Edit";
-import Details from "./pages/jobs/Details";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/todo-list" element={<List />} />
-          <Route path="/todo-list/edit/:id" element={<EditJob />} />
-          <Route path="/todo-list/details/:id" element={<Details />} />
         </Routes>
       </Router>
     </>
